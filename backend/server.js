dd1b17c29fdec76f835cc59ef77121673754ff7f
tiendungcln <tiendung98765@gmail.com>
2024-11-11 17:37:33 +0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRoute from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product', productRoute)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+
+// api endpoints
+app.get('/', (req, res) => {
+  res.send("API Working")
+})
+
+app.listen(port, () => console.log("Server started on PORT : " + port))
